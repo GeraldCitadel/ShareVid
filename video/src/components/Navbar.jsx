7110ad93d92e56e78b38ebebf7278c129ad8ec2f
Gerald Citadel <geraldcitadel@gmail.com>
2024-12-27 17:50:59 +0300

    changes on vercel

--- video/src/components/Navbar.jsx
+++ video/src/components/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
-  return (
-    <div className='flex justify-around px-10 pt-4 mb-11 text-white'>
+    return (
+        <div className='flex justify-around px-10 pt-4 mb-11 text-white'>
+            <div className='flex gap-2'>
+                <img className='w-9 h-9' src={logo} alt="logo" />
                 <h1 className='font-semibold text-2xl'><Link to='/'>ShareVid</Link></h1>
-                <ul className='flex  gap-3'>
-                    <NavLink to='/' className={({ isActive }) => isActive ? "button-text" : undefined} end>
-                        <li className='font-medium text-base'>Home</li>
-                    </NavLink>
-                    <NavLink to='/get-videos' className={({ isActive }) => isActive ? "button-text" : undefined}>
-                        <li className='font-medium text-base'>Videos</li>
-                    </NavLink>
-                </ul>
             </div>
-  )
+            <ul className='flex  gap-3'>
+                <NavLink to='/' className={({ isActive }) => isActive ? "button-text" : undefined} end>
+                    <li className='font-medium text-base'>Home</li>
+                </NavLink>
+                <NavLink to='/get-videos' className={({ isActive }) => isActive ? "button-text" : undefined}>
+                    <li className='font-medium text-base'>Videos</li>
+                </NavLink>
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
